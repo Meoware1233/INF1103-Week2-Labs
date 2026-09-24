952f1d39b6ea1aba276c4299449927295b834e00
Daniel Lim <daniellimzx@hotmail.com>
2026-09-24 16:56:47 +0800

    Added persistence in persistant_auditor file. It tracks the total existing file and history of every transaction

---
+++ persistent_auditor.py
@@ -0,0 +1,87 @@
+# 2601582 INF1103 Week 4 Lab
+def load_inventory():
+    inventory = 0
+    history = []
+    
+    try:
+        with open("inventory.txt", "r") as file:
+            # Read line 1 and remove from file
+            inventory = int(file.readline().strip())
+            for i in file:
+                # Read starting from line 2. 
+                history.append(int(i.strip()))
+            return inventory, history
+    except FileNotFoundError:
+        print("File not found, creating new file.")
+        with open("inventory.txt", "w") as file:
+            return inventory, history
+    except ValueError:
+        with open("inventory.txt", "w") as file:
+                    return inventory, history
+
+
+def save_inventory(inventory, history):
+    with open("inventory.txt", "w") as file:
+        file.write(str(inventory) + "\n")
+        for i in history:
+            file.write(str(i) + "\n")
+    print("Inventory sucessfully saved into inventory.txt")
+
+def get_valid_input(inventory, history):
+    rejected_entries = 0
+    while True:
+        if inventory > 500:
+            print("Inventory warning, inventory has exceeded 500 units. Exiting program.")
+            return inventory, history ,rejected_entries
+        else:
+            stock_quantity_input = input("Enter stock quantity (or type 'quit' to finish): ")
+            if stock_quantity_input == 'quit':
+                return inventory, history, rejected_entries
+            else:
+                try: 
+                    if int(stock_quantity_input) < 0:
+                        rejected_entries += 1
+                        print("Invalid negative input. Please enter a positive whole number for stock quantity.")
+                    else:
+                        inventory = inventory + int(stock_quantity_input)
+                        history.append(int(stock_quantity_input))
+                        print("Current inventory: ", inventory)
+                except ValueError:
+                    rejected_entries += 1
+                    print("Invalid text input. Please enter a positive whole number for stock quantity.")
+
+
+def process_delivery(current_total, new_value):
+    delivery_cost = current_total * new_value
+    return delivery_cost
+
+def calculate_tax(amount):
+    tax = 0.10 #10 percent tax
+    tax_amount = amount * tax
+    return tax_amount
+
+
+def generate_report(inventory, rejected_entries):
+    print("")
+    print(f'Description                        Qty/Cost')
+    print("-----------------------------------------------------------")
+    print(f'Total Units Processed               {inventory}')
+    print(f'Number of Failed/Rejected Entries   {rejected_entries}')
+    print(f'Cost of Delivery                    ${cost_of_delivery:.2f}')
+    print(f'Tax Cost                            ${tax_cost:.2f}')
+    print(f'Total cost with tax                 ${cost_of_delivery + tax_cost:.2f}')
+    print("-----------------------------------------------------------")
+
+
+exist_inventory, exist_history = load_inventory()
+inventory, history, rejected_entries = get_valid_input(exist_inventory, exist_history)
+
+# Example usage of process_delivery function with cost of delivery being $10
+cost_of_delivery = process_delivery(inventory, 10)  
+
+tax_cost = calculate_tax(cost_of_delivery)
+
+# generate_report(inventory, rejected_entries, cost_of_delivery, tax_cost)
+generate_report(inventory, rejected_entries)
+
+save_inventory(inventory, history)
